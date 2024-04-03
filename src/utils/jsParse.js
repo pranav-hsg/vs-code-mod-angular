0d200c769dcc1d931e5331183daaf333fd882e9f
pranav <pranav.hs@sen-sei.in>
2024-04-03 21:04:15 +0530

    comments added

--- src/utils/jsParse.js
+++ src/utils/jsParse.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ParseJS = void 0;
+// Class which is used to point out js functions and methods. It can analyze a ".ts / .js" file to spot particular methods 
+// This can be used to remove methods or add methods.
 class ParseJS {
     constructor() {
+        // select everything including space and new line, non-space and space regex string.
         this.any = `[.\\n\\S\\s]*?`;
+        // select everything including space and new line regex string leaving non-space.
         this.any2 = `[.\\n\\s]*?`;
         this.nSpace = `[\\s\\n]*?`;
         this.closeTag = `[>\\n\\s\\t]+?`;
@@ -14,16 +18,21 @@ class ParseJS {
     }
     matchWithErrorHandle(str, regStr, flags = "") {
         try {
+            // match string with regexp with flags
             let res = str.match(new RegExp(regStr, flags));
+            // If you get match return string else return empty string
             res = res ? res[0] : "";
+            // cache the string
             this.cacheStr = res;
             return res;
         }
         catch (e) {
             console.warn(e);
         }
+        return "";
     }
     matchWithErrorHandleIndex(str, regStr, flags = "") {
+        // similar as matchWithErrorHandle but returns entire match instead of string
         try {
             let res = str.match(new RegExp(regStr, flags));
             res = res ? res : "";
@@ -34,12 +43,14 @@ class ParseJS {
             console.warn(e);
         }
     }
+    // Returns match string array else returns null
     matchAll(str, regex) {
         let match = str.match(new RegExp(regex, "g"));
         return match;
     }
     getMatchCounts(str, regex) {
-        let regexMatchArray = this.matchAll(str, regex);
+        // Gets match count
+        let regexMatchArray = this.matchAll(str, regex); // gets Array<string>;
         if (regexMatchArray)
             return regexMatchArray.length;
         return 0;
@@ -64,10 +75,15 @@ class ParseJS {
         return [match, replacedStr];
     }
     getSpaceBeforeFunctionRegex(name) {
+        // Capture number of spaces before a method/function
+        // name is a function or method name
         return `^[^\n]*?(?=${name + this.any}\\(${this.any}\\)${this.any}\\{${this.any}$)`;
     }
     getStartingSpacesOfFunc(str, name) {
+        // Get regexp for space match 
+        // returns like "    ";
         let regex = this.getSpaceBeforeFunctionRegex(name);
+        // multiline match
         let match = str.match(new RegExp(regex, "m"));
         if (match) {
             return match[0].split(" ").length - 1;
@@ -77,45 +93,64 @@ class ParseJS {
         }
     }
     getOCRegex(nSpace, name) {
+        // name -> function name 
+        // space before function starting.
+        // Spaces regex likes
+        // spaces<Function-Name>{<anything>
+        // spaces}
+        // This is the main regex to select / detect function
         return ("^" +
             "\\s".repeat(nSpace) +
             `${name}[.\\n\\s\\t]*?[\\S]{0,3}[.\\n\\s\\t]*?\\{[.\\n\\s\\S\\t]*?[.\\n\\s\\S\\t]*?^${"\\s".repeat(nSpace)}\\}`);
     }
     getMethodRegex(html, name) {
+        // Number of starting spaces before function
         let n = this.getStartingSpacesOfFunc(html, name);
+        // Get method regex by passing number of spaces
         let regex = this.getOCRegex(n, name);
         return regex;
     }
     getMethod(html, name) {
+        // Capture method by name. Basically it grabs by getting spaces before(start) and after function(end).
         let regex = this.getMethodRegex(html, name);
+        // Multiline match to make '^' work on each line in regexp
+        // There are many "^"(start of line) in ocregex if multi line mode is not there the "^" only works on start of entire string instead of matching working on each line of entire string multi line.
         let match = this.matchWithErrorHandle(html, regex, "m");
         return match;
     }
     replaceMethod(html, name, repMethod) {
+        // First get regular expression for a particular method
         let regex = this.getMethodRegex(html, name);
+        // Replace the method with replace method
         let match = this.replaceWithErrorHandle(html, regex, "m", repMethod);
+        // Return replaced version of string( Here string may include entire file).
         return match;
     }
     insert(str, index, value) {
         return str.substr(0, index) + value + str.substr(index);
     }
     findClassEnd(text) {
+        // Find end of particular class 
         let lastBrace = text.lastIndexOf('}');
         return lastBrace ? lastBrace : -1;
     }
     findClassStart(text) {
+        // Find the start of class in string.(Entire file contents)
         let classStart = this.matchWithErrorHandleIndex(text, `(?<=class${this.any}\\{)[.\\n\\s\\S]`);
         return classStart ? classStart.index : -1;
     }
     addMethod(text, method) {
+        // Add method to end of class
         let i = this.findClassEnd(text);
         return this.insert(text, i, '\n' + method);
     }
     addAttribute(text, attr) {
+        // Add attribute at the start of the class
         let i = this.findClassStart(text);
         return this.insert(text, i, '\n' + attr);
     }
     removeMethod(text, name) {
+        // Remove methods
         let regex = this.getMethodRegex(text, name);
         let repStr = this.replaceWithErrorHandle(text, regex, "m", '');
         return repStr;
